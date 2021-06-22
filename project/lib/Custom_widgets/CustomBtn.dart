2da941f31b8d3e1a10d1c334159acb38e43bd319
Fahad Shaikh <fs05452@st.habib.edu.pk>
2021-06-22 18:25:56 +0500

    Added Material Touch Ripples and Sound
    Swapped Gesture Detector with the Ink Well widget - entailed a reordering of the widgets and the wrapping of the Ink Well in a Material Widget.

--- project/lib/Custom_widgets/CustomBtn.dart
+++ project/lib/Custom_widgets/CustomBtn.dart
@@ -42,58 +42,59 @@ class Button extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.fromLTRB(lp, tp, rp, bp),
-      child: Container(
-        height: h,
-        width: w,
-        child: GestureDetector(
-          onTap: meth,
-          child: Container(
-            decoration: BoxDecoration(
-              boxShadow: [
-                //background color of box
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 5.0, // soften the shadow
-                  spreadRadius: 2.0, //extend the shadow
-                  offset: Offset(
-                    2.0, // Move to right 10  horizontally
-                    2.0, // Move to bottom 10 Vertically
-                  ),
-                )
-              ],
-              border: Border.all(
-                color: Color(0xffba2529),
-                style: BorderStyle.solid,
-                width: 1.0,
-              ),
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20.0),
+        padding: EdgeInsets.fromLTRB(lp, tp, rp, bp),
+        child: Container(
+          height: h,
+          width: w,
+          decoration: BoxDecoration(
+            boxShadow: [
+              //background color of box
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 5.0, // soften the shadow
+                spreadRadius: 2.0, //extend the shadow
+                offset: Offset(
+                  2.0, // Move to right 10  horizontally
+                  2.0, // Move to bottom 10 Vertically
+                ),
+              )
+            ],
+            border: Border.all(
+              color: Color(0xffba2529),
+              style: BorderStyle.solid,
+              width: 1.0,
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Center(
-                  child: Text(
-                    text,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'SanFrancisco',
-                      fontSize: fontSize,
-                      fontWeight: fw,
-                      letterSpacing: 0.5,
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Material(
+            color: Colors.white.withOpacity(0.0),
+            child: InkWell(
+              splashColor: Colors.red.shade200,
+              onTap: meth,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: Text(
+                      text,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'SanFrancisco',
+                        fontSize: fontSize,
+                        fontWeight: fw,
+                        letterSpacing: 0.25,
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 2,
-                ),
-                _icon(ic),
-              ],
+                  SizedBox(
+                    height: 2,
+                  ),
+                  _icon(ic),
+                ],
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
